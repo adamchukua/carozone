85698bb2e3bf9556305c3de9ab48e68c51a05954
thegradle <adamchuk.v.1@gmail.com>
2023-06-30 12:36:48 +0300

    Add table with data from api

--- src/components/CarTable.js
+++ src/components/CarTable.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -6,52 +7,58 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const cars = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
+import Pagination from '@mui/material/Pagination';
 
 export default function CarTable() {
+  const [cars, setCars] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://myfakeapi.com/api/cars/")
+      .then(response => {
+        setCars(response.data.cars);
+      })
+      .catch(error => {
+        console.error("Error fetching cars: ", error);
+      });
+  }, []);
+
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Company</TableCell>
-            <TableCell align="right">Model</TableCell>
-            <TableCell align="right">VIN</TableCell>
-            <TableCell align="right">Color</TableCell>
-            <TableCell align="right">Year</TableCell>
-            <TableCell align="right">Price</TableCell>
-            <TableCell align="right">Availability</TableCell>
-            <TableCell align="right">Actions</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {cars.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+    <>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Company</TableCell>
+              <TableCell align="right">Model</TableCell>
+              <TableCell align="right">VIN</TableCell>
+              <TableCell align="right">Color</TableCell>
+              <TableCell align="right">Year</TableCell>
+              <TableCell align="right">Price</TableCell>
+              <TableCell align="right">Availability</TableCell>
+              <TableCell align="right">Actions</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+          {cars.map((row) => (
+              <TableRow
+                key={row.id}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">{row.car}</TableCell>
+                <TableCell align="right">{row.car_model}</TableCell>
+                <TableCell align="right">{row.car_vin}</TableCell>
+                <TableCell align="right">{row.car_color}</TableCell>
+                <TableCell align="right">{row.car_model_year}</TableCell>
+                <TableCell align="right">{row.price}</TableCell>
+                <TableCell align="right">{row.availability}</TableCell>
+                <TableCell align="right">Edit Delete</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+
+      <Pagination count={10} />
+    </>
   )
 }
